4fd65a7cb441029c65a478fa636cadb2aa6ce935
Sam Foxman <samfoxman320@gmail.com>
2020-08-11 23:52:19 -0400

    Remove checks because they were causing trouble https://github.com/archlinux/svntogit-community/blob/53b854bb71003c64c39b5bc6823df8e0ff2a712d/trunk/0002-Remove-GNU-branding.patch

--- run-many.sh
+++ run-many.sh
@@ -3,18 +3,6 @@
 set -e
 set -o pipefail
 
-if ! command -v parallel > /dev/null; then
-  echo 'missing gnu parallel'
-  echo 'sudo apt-get install parallel'
-  exit 1
-fi
-
-if ! parallel --help | grep -q 'GNU Parallel'; then
-  echo 'found parallel, but it is not gnu parallel'
-  echo "\`sudo apt-get install parallel\` is the right package"
-  exit 1
-fi
-
 TOTAL_EXECUTIONS="${1:-1000}"
 CONCURRENT_JOBS="${2:-"$(( "$(nproc)" * 2 ))"}"
 
